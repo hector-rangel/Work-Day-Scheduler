d506c6e4e04affe25c15734def46e2edbf4680e6
Hector Rangel <hector.rangeljr@yahoo.com>
2021-04-25 21:22:40 -0500

    added a function tracker to add/remove classes based on hour

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,17 @@
 // displayers current day at the top of page.
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"));
+$("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm a"));
 
+// saveBtn function
 $(".saveBtn").on("click", function () {
     console.log(this);
     var text = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
-    // local storage
+    // localStorage.setItem
     localStorage.setItem(time, text);
 })
 
+// setting local storage for each hour
 $("#hour9 .description").val(localStorage.getItem("hour9"));
 $("#hour10 .description").val(localStorage.getItem("hour10"));
 $("#hour11 .description").val(localStorage.getItem("hour11"));
@@ -20,12 +22,31 @@ $("#hour15 .description").val(localStorage.getItem("hour15"));
 $("#hour16 .description").val(localStorage.getItem("hour16"));
 $("#hour17 .description").val(localStorage.getItem("hour17"));
 
-function tracker() {
-    var currentHour = moment().hour(); 
+// function to add/remove classes
 
+function tracker() {
+    var currentHour = moment().hour();
     // loop
-    $(".time-block").each(function() {
+    $(".time-block").each(function () {
         var blockHour = parseInt($(this).attr("id").split("hour")[1]);
-        console.log(blockHour,currentHour);
+        console.log(blockHour, currentHour);
+        
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+            console.log(this);
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
     })
 }
+tracker();
